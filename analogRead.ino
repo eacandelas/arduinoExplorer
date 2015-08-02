c59fc5bf664d8e6c58fff8bd54e96def6693324a
Eden Candelas <e.candelas@gmail.com>
2015-08-02 10:44:54 -0500

    added analogRead

---
+++ analogRead.ino
@@ -0,0 +1,16 @@
+#include "Arduino.h"
+
+int lectura;
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  lectura = analogRead(0);
+  Serial.print("Lectura: ");
+  Serial.println(lectura);
+  delay(1000);
+}
